370fa0e9bc31856823e7b86ea4f23232250ab46e
algobardo <mezzetti@gmail.com>
2015-09-14 20:44:12 +0200

    Initial commit

---
+++ lib/dart_datalog.dart
@@ -0,0 +1,11 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+/// The dart_datalog library.
+///
+/// This is an awesome library. More dartdocs go here.
+library dart_datalog;
+
+// TODO: Export any libraries intended for clients of this package.
+
+export 'src/dart_datalog_base.dart';
